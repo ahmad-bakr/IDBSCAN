17cb573b0bc65c4d496ff4b848afa709000c26dc
ahmad bakr <ahmad.m.bakr@gmail.com>
2012-03-27 09:08:58 +0200

    calculate the distance matrix

--- src/clustering/algorithms/DBSCAN.java
+++ src/clustering/algorithms/DBSCAN.java
@@ -8,6 +8,40 @@ import datasets.DatasetsIF;
 
 public class DBSCAN {
 
+	
+	private Hashtable<DatasetPoint, ArrayList<Double>> distanceMatrix;
+	
+	public DBSCAN() {
+		this.distanceMatrix = new Hashtable<DatasetPoint, ArrayList<Double>>();
+	}
+	
+	/**
+	 * calculate the distance matrix between all the points
+	 * @param dataset dataset
+	 */
+	public void calculateDistanceMatrix(ArrayList<DatasetPoint> dataset){
+		for (int i = 0; i < dataset.size(); i++) {
+			DatasetPoint currentPoint = dataset.get(i);
+			ArrayList<Double> distanceRecord = new ArrayList<Double>();
+			for (int j = 0; j < dataset.size(); j++) {
+				distanceRecord.add(calculateDistanceBtwTwoPoints(currentPoint, dataset.get(j)));
+			}
+			this.distanceMatrix.put(currentPoint, distanceRecord);
+		}
+	}
+	
+	/**
+	 * calculate the Euclidean Distance between two points
+	 * @param p1 point 1
+	 * @param p2 point 2
+	 * @return Distance
+	 */
+	public double calculateDistanceBtwTwoPoints(DatasetPoint p1, DatasetPoint p2){
+		double xDiff = p1.getX() - p2.getX();
+		double yDiff = p1.getY() - p2.getY();
+		return Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+	}
+	
 	/**
 	 * Run DBSCAN algorithm
 	 * @param dataset datasets of points
@@ -15,6 +49,7 @@ public class DBSCAN {
 	 * @param minPts min pts value
 	 */
 	public void run(ArrayList<DatasetPoint> dataset, double eps, int minPts){
+		calculateDistanceMatrix(dataset);
 		int clusterLabel = 0;
 		for (int i = 0; i < dataset.size(); i++) {
 			DatasetPoint point = dataset.get(i);
