77670624af4e42fee457c9b031940c236ab2b75b
ahmad bakr <ahmad.m.bakr@gmail.com>
2012-04-02 22:48:23 +0200

    start implementing CLARANS

--- src/clustering/partitioning/Clarans.java
+++ src/clustering/partitioning/Clarans.java
@@ -1,5 +1,25 @@
 package clustering.partitioning;
 
+import java.util.ArrayList;
+
+import datasets.DatasetPoint;
+
 public class Clarans {
+	
+	public void perform(ArrayList<DatasetPoint> dataset, int numLocals, int maxNeighbors, int numPartitions){
+		int i=0;
+		int j=1;
+		double minCost = Double.MAX_VALUE;
+		NodeFactory graph = new NodeFactory(dataset, numPartitions);
+		Node current = graph.getNode();
+		Node randomNeighbor = graph.getNeighbor(current);
+		double currentCost = calculateCost(current);
+		double neighborCost = calculateCost(randomNeighbor);
+		
+	}
+	
+	public double calculateCost(Node node){
+		return 0;
+	}
 
 }
