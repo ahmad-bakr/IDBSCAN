d74e767f12584e189aa0fb818366cc2f32a13872
ahmad bakr <ahmad.m.bakr@gmail.com>
2012-04-06 16:05:06 +0200

    start merging step

--- src/clustering/algorithms/EnhancedDBSCAN.java
+++ src/clustering/algorithms/EnhancedDBSCAN.java
@@ -1,5 +1,33 @@
 package clustering.algorithms;
 
-public class EnhancedDBSCAN {
+import java.util.ArrayList;
 
+import clustering.partitioning.Clarans;
+import clustering.partitioning.Node;
+
+import datasets.DatasetPoint;
+
+public class EnhancedDBSCAN {
+	
+	private ArrayList<DatasetPoint> dataset;
+	
+	public EnhancedDBSCAN(ArrayList<DatasetPoint> dataset) {
+		this.dataset = dataset;
+	}
+	
+	public void run(int numLocals, int maxNeighbors, int numPartitions, double eps, int minPts, double alpha){
+		Clarans clarans = new Clarans();
+		Node  bestRanSolution = clarans.perform(dataset, numLocals, maxNeighbors, numPartitions);
+		for (int i = 0; i < bestRanSolution.getMedoids().length; i++) {
+			DBSCANPartitioner dbscanpart = new DBSCANPartitioner(dataset, i, bestRanSolution.getMedoidsAssignedPoints().get(i));
+			dbscanpart.run(eps, minPts);			
+		}
+		mergeRegions(alpha);
+	}
+	
+	public void mergeRegions(double alpha){
+		
+	}
+	
+	
 }
